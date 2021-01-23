13dc94d92fd99dfb0aff553e59681aaf9da0c598
Chris Eykamp <chris@eykamp.com>
2021-01-22 16:48:05 -0800

    Implement retrieve from database

--- app.py
+++ app.py
@@ -3,8 +3,7 @@
 
 # View logs: heroku logs --tail --remote pro
 
-# curl -F "file=.gitignore" localhost:5000/put -H "key: 12345"
-# curl -F "file=.gitignore" https://firmware-update-service.herokuapp.com/put -H "key: 12345"
+# curl -F "firmware=@filename" -H "key: 12345" https://firmware-update-service.herokuapp.com/put
 
 """
 Start postgres from cmd:
@@ -46,7 +45,10 @@ def welcome():
 # Same as update but without the version checks
 @app.route("/test-get/<name>", methods=["GET"])
 def test_update(name):
-    firmware = get_firmware(FIRMWARE_LOCATION + "/" + name)
+    try:
+        firmware = get_firmware_from_database(name)
+    except Exception as ex:
+        return str(ex), 422
 
     print("Sending firmware")
     return Response(firmware.data, mimetype="application/octet-stream", headers={"X-MD5": firmware.md5})
@@ -59,53 +61,16 @@ def app_exists(cur, key: str) -> bool:
     return cur.fetchone() is not None
 
 
-@app.route("/put", methods=["POST"])
-def upload_firmware():
-    if "key" not in request.headers:
-        return "Missing key", 422
-
-    key = request.headers["key"]        # Apparently case insensitive
-    cur = conn.cursor()
-
-    # Is this a known key?
-    if not app_exists(cur, key):
-        return "Bad key", 404
-
-    # Upload with filename "file"
-    if "file" not in request.files:
-        print(f"Missing file: request.files = {request.files}", flush=True)
-        return "Missing file", 422
-
-    file = request.files["file"]
-    if file.filename == "":
-        return "Empty filename", 422
-
-    # Delete any existing firmware records
-    query = "DELETE FROM firmware WHERE key = %s;"
-    cur.execute(query, [key])
-
-    # Insert new firmware record
-    firmware = file.stream.read()
-
-    if len(firmware) > 3 * MEGABYTES:
-        return "Too big", 400
-
-
-    md5 = hashlib.md5(firmware).hexdigest()
-    query = "INSERT INTO firmware (key, md5, firmware) VALUES(%s, %s, %s);"
-    cur.execute(query, [key, md5, firmware])
-
-    conn.commit()       # Make insert permanent
-
-    return "Ok", 200
-
-
 
 @app.route("/get/<name>", methods=["GET"])
 def update(name):
-    client_md5 = request.headers["HTTP_X_ESP8266_SKETCH_MD5"]
 
-    firmware = get_firmware(name)
+    try:
+        firmware = get_firmware_from_database(name)
+    except Exception as ex:
+        return str(ex), 422
+
+    client_md5 = request.headers["HTTP_X_ESP8266_SKETCH_MD5"]
     if client_md5 == firmware.md5:
         # Already have most recent firmware
         return "", 302
@@ -129,12 +94,70 @@ def update(name):
     # 'HTTP_X_ESP8266_VERSION': '0.120',
 
 
-def get_firmware(name: str) -> HashedFirmware:
-    with open(firmware_path, "rb") as f:
-        bin_image = f.read()
-    md5 = hashlib.md5(bin_image).hexdigest()
-    print("Found firmware bytes={} md5={}".format(len(bin_image), md5))
-    return HashedFirmware(bin_image, md5)
+@app.route("/put", methods=["POST"])
+def upload_firmware():
+    """
+    Insert a new copy of the firmware into the database. Need to provide a file
+    called "firmware" and a header called "key" with the secret app key in it.
+
+    curl -F "firmware=@filename" -H "key: 12345" https://firmware-update-service.herokuapp.com/put
+    """
+    if "key" not in request.headers:
+        return "Missing key", 422
+
+    key = request.headers["key"]        # Apparently case insensitive
+    cur = conn.cursor()
+
+    # Is this a known key?
+    if not app_exists(cur, key):
+        return "Bad key", 404
+
+    # Upload with filename "file"
+    if "firmware" not in request.files:
+        print(f"Missing file: request.files = {request.files}", flush=True)
+        return "Missing file", 422
+
+    file = request.files["firmware"]
+    if file.filename == "":
+        return "Empty filename", 422
+
+    # Delete any existing firmware records
+    query = "DELETE FROM firmware WHERE key = %s;"
+    cur.execute(query, [key])
+
+    # Insert new firmware record
+    firmware = file.stream.read()
+
+    if len(firmware) > 3 * MEGABYTES:
+        return "Too big", 400
+
+    md5 = hashlib.md5(firmware).hexdigest()
+    query = "INSERT INTO firmware (key, md5, firmware) VALUES(%s, %s, %s);"
+    cur.execute(query, [key, md5, firmware])
+
+    conn.commit()       # Make insert permanent
+
+    return "Ok", 200
+
+
+def get_firmware_from_database(name: str) -> HashedFirmware:
+    cur = conn.cursor()
+
+    query = "SELECT key FROM app firmware WHERE key = %s"
+    cur.execute(query, [name])
+
+    query = "SELECT md5, firmware FROM firmware WHERE key = %s"
+    row = cur.fetchone()
+    if not row:
+        print(f"Cound not find name {name}", flush=True)
+        raise Exception("Could not find name")
+
+    key = row[0]
+
+    row = cur.execute(query, [key])
+    if not row:
+        print(f"No firmware for key {key}", flush=True)
+        raise Exception("No firwmare for key")
 
 
 
